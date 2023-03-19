62bd497c083a19bdd57842d4f5603f81f81bd840
aavidan <amir.avidan@beyondminds.ai>
2023-03-19 14:15:57 +0200

    Test for deploying server

--- app.js
+++ app.js
@@ -92,7 +92,3 @@ server.post('/location', async (req,res) => {
     const distance = checkforUserDistance(req.body.location);
     res.json({distance})
 })
-
-
-/* Start listening at your defined PORT */
-server.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
