b5e2883a85b6e6cb3ed81805de86cc6e510f79eb
harelfogel <fogell06@gmail.com>
2023-04-12 16:00:48 +0300

    adding endpoint to the ML server

--- sensorValues.service.js
+++ sensorValues.service.js
@@ -1,17 +1,17 @@
 const SensorValue = require('./SensorValue');
 
-async function getLatestTemperatureAndHumidity() {
+async function getLatestSensorValues() {
     try {
         const temperature = await SensorValue.findOne({ sensor_type: 'temperature' }).sort({ timestamp: -1 }).exec();
         const humidity = await SensorValue.findOne({ sensor_type: 'humidity' }).sort({ timestamp: -1 }).exec();
-        return { temperature: temperature.value, humidity: humidity.value };
+        const distance= await SensorValue.findOne({sensor_type:'distance'}).sort({timestamp:-1}).exec();
+        return { temperature: temperature.value, humidity: humidity.value,distance:distance.value};
     } catch (error) {
-        console.error(`Error fetching latest temperature and humidity: ${error}`);
+        console.error(`Error fetching latest temperature and humidity and distance: ${error}`);
         throw error;
     }
 }
 
-
 module.exports = {
-    getLatestTemperatureAndHumidity
+    getLatestSensorValues
 }
