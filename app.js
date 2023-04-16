9e8fa105d28a6085e5bf752efbec635b15f92b78
Yovel Elad <yuvel.elad@gmail.com>
2023-04-16 09:08:54 +0300

    add endpoint to update suggestion

--- app.js
+++ app.js
@@ -1,32 +1,59 @@
-require('dotenv').config();
-const getClientDetails = require('./smartThings.js');
-const express = require('express');
-const connectDB = require('./config');
-const { switchAcState, getAcState, getSensiboSensors, parseSensorAndWriteToMongo, removeAllSensorValues } = require('./sensibo.js');
+require("dotenv").config();
+const getClientDetails = require("./smartThings.js");
+const express = require("express");
+const connectDB = require("./config");
+const {
+  switchAcState,
+  getAcState,
+  getSensiboSensors,
+  parseSensorAndWriteToMongo,
+  removeAllSensorValues,
+} = require("./sensibo.js");
 const cors = require("cors");
-const { homeConnectAuth, homeConnectToken } = require('./homeConnect.js');
-const { smartThingsGetDevices, switchWasherWater } = require('./smartThings2.js');
-const { checkforUserDistance } = require('./location.js');
-const { removeSensorValueByType, getFunctionsFromDB, getHeaterState, activateDevices } = require('./common.js');
-const { insertRuleToDB, getAllRules, setRuleActive, deleteRuleById } = require('./rules.service.js');
-const { switchHeaterState } = require('./heaterController.js');
+const { homeConnectAuth, homeConnectToken } = require("./homeConnect.js");
+const {
+  smartThingsGetDevices,
+  switchWasherWater,
+} = require("./smartThings2.js");
+const { checkforUserDistance } = require("./location.js");
+const {
+  removeSensorValueByType,
+  getFunctionsFromDB,
+  getHeaterState,
+  activateDevices,
+} = require("./common.js");
+const {
+  insertRuleToDB,
+  getAllRules,
+  setRuleActive,
+  deleteRuleById,
+} = require("./rules.service.js");
+const { switchHeaterState } = require("./heaterController.js");
 
-const { getSuggestions, addSuggestionsToDatabase, updateSuggestions } = require('./suggestions.service.js');
-const { getDevices } = require('./devices.service.js');
-const { callBayesianScript, runBayesianScript, addingDataToCsv } = require('./machineLearning.js');
-const { getCurrentSeasonAndHour } = require('./time.service.js');
-const { signInUser, registerUser } = require('./users.service');
-const jwt = require('jsonwebtoken');
-const axios=require('axios');
-const schedule = require('node-schedule');
+const {
+  getSuggestions,
+  addSuggestionsToDatabase,
+  updateSuggestions,
+  addSuggestionMenually,
+} = require("./suggestions.service.js");
+const { getDevices } = require("./devices.service.js");
+const {
+  callBayesianScript,
+  runBayesianScript,
+  addingDataToCsv,
+} = require("./machineLearning.js");
+const { getCurrentSeasonAndHour } = require("./time.service.js");
+const { signInUser, registerUser } = require("./users.service");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const schedule = require("node-schedule");
 
-const connectToWs = require('./ws.js');
+const connectToWs = require("./ws.js");
 
-const { getLatestSensorValues } = require('./sensorValues.service.js');
-const { response } = require('express');
+const { getLatestSensorValues } = require("./sensorValues.service.js");
+const { response } = require("express");
 
-require('dotenv').config();
-
+require("dotenv").config();
 
 const server = express();
 const port = process.env.PORT || 3001;
@@ -37,26 +64,21 @@ server.use(cors({ origin: true }));
 connectDB();
 
 connectToWs();
-  
-
-
-
 
 //Handle get requests
-server.get('/', function (req, res) {
+server.get("/", function (req, res) {
   res.json({ message: `Welcome to SmartByte server` });
-})
+});
 
 /* Handle POST requests */
-server.post('/', function (req, res, next) {
+server.post("/", function (req, res, next) {
   console.log(req);
   smartapp.handleHttpCallback(req, res);
 });
 
-
 // --------------------------------- Sign up ---------------------------------
 
-server.post('/register', async (req, res) => {
+server.post("/register", async (req, res) => {
   const { fullName, email, password, role } = req.body;
   const response = await registerUser(fullName, email, password, role);
   res.status(response.status).json({ message: response.message });
@@ -64,60 +86,62 @@ server.post('/register', async (req, res) => {
 
 // --------------------------------- Sign in ---------------------------------
 
-server.post('/login', async (req, res) => {
+server.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const response = await signInUser(email, password);
 
   if (response.status === 200) {
-    res.status(200).json({ message: response.message, token: response.token, user: response.user });
+    res
+      .status(200)
+      .json({
+        message: response.message,
+        token: response.token,
+        user: response.user,
+      });
   } else {
     res.status(response.status).json({ message: response.message });
   }
 });
 
 // --------------------------------- test yovel ---------------------------------
-server.post('/test', async (req, res) => {
+server.post("/test", async (req, res) => {
   let response;
-  try{
-    response = await activateDevices(req.body.func)
+  try {
+    response = await activateDevices(req.body.func);
     // console.log({response})
     res.status(response.statusCode).json(response.data);
-
   } catch (err) {
     res.status(400).json(response.data);
-
   }
 });
 
-
-
 // --------------------------------- Rules ---------------------------------
-server.get('/rules', async (req, res) => {
+server.get("/rules", async (req, res) => {
   const response = await getAllRules();
   res.status(response.statusCode).json(response.data);
 });
 
 // Define the route for adding a new rule
-server.post('/rules', async (req, res) => {
-  console.log(req.body)
+server.post("/rules", async (req, res) => {
+  console.log(req.body);
   const { rule, isStrict } = req.body;
   const response = await insertRuleToDB(rule, isStrict);
-  res.status(response.statusCode).send(response.message)
+  res.status(response.statusCode).send(response.message);
 });
 
-server.post('/rules/:id', async (req, res) => {
+server.post("/rules/:id", async (req, res) => {
   const { isActive } = req.body;
   const id = req.params.id;
   const response = await setRuleActive(id, isActive);
   return res.status(response.statusCode).send(response.message);
 });
 
-server.delete('/rules/:id', async (req, res) => {
+server.delete("/rules/:id", async (req, res) => {
   const id = req.params.id;
   try {
     // Assuming you have a function to delete the rule by its ID
     const response = await deleteRuleById(id);
-    console.log({response});
+    console.log({ response });
 
     if (response.status === 200) {
       res.status(200).json({ message: "Rule deleted successfully" });
@@ -129,114 +153,114 @@ server.delete('/rules/:id', async (req, res) => {
   }
 });
 
-
-
 // --------------------------------- SmartThings- Laundry ---------------------------------
 
 //Handle get requests
-server.get('/smartthings', function (req, res) {
+server.get("/smartthings", function (req, res) {
   getClientDetails();
   res.json({ message: `Welcome to smartthings details` });
 });
 
-server.get('/homeConnect', (req, res) => {
+server.get("/homeConnect", (req, res) => {
   homeConnectAuth();
-  res.json({ message: 'Welcome to Home Connect' })
-})
+  res.json({ message: "Welcome to Home Connect" });
+});
 
-server.get('/homeConnect/callback', (req, res) => {
+server.get("/homeConnect/callback", (req, res) => {
   // console.log("callback", req.query)
   homeConnectToken(req, res);
-  res.json({ message: 'token' })
-})
-
+  res.json({ message: "token" });
+});
 
 // --------------------------------- Sensibo- AC ---------------------------------
 
-server.post('/sensibo', async (req, res) => {
-  try{
-
-    console.log("-----------sensibo---------------")
+server.post("/sensibo", async (req, res) => {
+  try {
+    console.log("-----------sensibo---------------");
     const state = req.body.state;
     const temperature = req.body.temperature || null;
-    console.log({state, temperature})
+    console.log({ state, temperature });
     await switchAcState(state, temperature);
-    res.json({ statusCode: 200 })
-  } catch(err) {
-    return res.status(400).json({ message: err.message })
+    res.json({ statusCode: 200 });
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
   }
-})
+});
 
-server.get('/sensibo', async (req, res) => {
-  console.log("sensibo get acState")
+server.get("/sensibo", async (req, res) => {
+  console.log("sensibo get acState");
   const state = await getAcState();
-  res.json({ state })
-})
+  res.json({ state });
+});
 
-server.get('/temperature', async (req, res) => {
+server.get("/temperature", async (req, res) => {
   const response = await getSensiboSensors();
   res.json(response.data.result);
-})
-
+});
 
 // --------------------------------- Tuya- Heater ---------------------------------
 
-server.post('/heater', async (req, res) => {
+server.post("/heater", async (req, res) => {
   const { value } = req.body;
   const response = await switchHeaterState(value);
-  res.json({ response })
-})
+  res.json({ response });
+});
 
-server.get('/smartthings/v2/devices', async (req, res) => {
-  const deviceId = req.query.deviceId || '';
+server.get("/smartthings/v2/devices", async (req, res) => {
+  const deviceId = req.query.deviceId || "";
   const response = await smartThingsGetDevices(deviceId);
-  res.json(response)
-})
+  res.json(response);
+});
 
-
-server.post('/smartthings/v2/switch', async (req, res) => {
-  console.log("smart things SWITCH", req.body.state)
+server.post("/smartthings/v2/switch", async (req, res) => {
+  console.log("smart things SWITCH", req.body.state);
 
   // const response = await switchWasherWater(req.body.state)
-})
+});
 
-server.post('/smartthings/v2/devices/:deviceId/switch', async (req, res) => {
-  const deviceId = req.url.split('/')[4];
+server.post("/smartthings/v2/devices/:deviceId/switch", async (req, res) => {
+  const deviceId = req.url.split("/")[4];
   const status = req.body.status;
-  const response = await switchWasherWater(deviceId, status)
-  res.json(response.data)
-})
+  const response = await switchWasherWater(deviceId, status);
+  res.json(response.data);
+});
 
 // --------------------------------- Location ---------------------------------
 
-server.post('/location', async (req, res) => {
+server.post("/location", async (req, res) => {
   // console.log(req.body)
   const distance = await checkforUserDistance(req.body.location);
-  res.json({ distance })
-})
+  res.json({ distance });
+});
 // --------------------------------- Devices ---------------------------------
 
-server.get('/devices', async (req, res) => {
+server.get("/devices", async (req, res) => {
   const devices = await getDevices();
   return res.json(devices);
-})
+});
 
 // --------------------------------- Machine Learnign-Recoomnadations ---------------------------------
-server.get('/update_data', async (req,res) => {
+server.get("/update_data", async (req, res) => {
   try {
     const adding_res = await addingDataToCsv();
     res.status(200).json(adding_res);
   } catch (error) {
     console.error(`Error adding data: ${error}`);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
-})
+});
 
 // --------------------------------- Machine Learnign-Recoomnadations ---------------------------------
-server.get('/recommend_device', async (req, res) => {
+server.get("/recommend_device", async (req, res) => {
   try {
-    const devices=["heater_switch","lights","ac_status","fan","laundry_machine"];
-    const {temperature,humidity,distance}= await getLatestSensorValues();
+    const devices = [
+      "heater_switch",
+      "lights",
+      "ac_status",
+      "fan",
+      "laundry_machine",
+    ];
+    const { temperature, humidity, distance } = await getLatestSensorValues();
     const { season, hour } = getCurrentSeasonAndHour();
     const requestData = {
       devices,
@@ -251,29 +275,31 @@ server.get('/recommend_device', async (req, res) => {
     res.json(recommendation);
   } catch (error) {
     console.error(`Error getting recommendation: ${error}`);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
-
 // --------------------------------- Suggestions ---------------------------------
-server.get('/suggestions', async (req, res) => {
+server.get("/suggestions", async (req, res) => {
   try {
     const suggestions = await getSuggestions();
     res.status(200).json(suggestions);
   } catch (err) {
-    res.status(500).json({ message: 'Error fetching suggestions' });
+    res.status(500).json({ message: "Error fetching suggestions" });
   }
 });
 
 // --------------------------------- Insights-Graph Data ---------------------------------
-server.get('/graph-data', async (req, res) => {
+server.get("/graph-data", async (req, res) => {
   try {
     const device = req.query.device;
     const time_range = req.query.time_range;
-    const response = await axios.get(`${process.env.PYTHON_SERVER_URL}/graph-data`, {
-      params: { device, time_range },
-    });
+    const response = await axios.get(
+      `${process.env.PYTHON_SERVER_URL}/graph-data`,
+      {
+        params: { device, time_range },
+      }
+    );
     res.status(200).json(response.data);
   } catch (error) {
     console.error("Error fetching graph data:", error);
@@ -281,36 +307,39 @@ server.get('/graph-data', async (req, res) => {
   }
 });
 
-server.put('/suggestions', async (req, res) => {
+server.put("/suggestions", async (req, res) => {
   try {
     // const id = req.params.id;
-    Object.entries(req.body).map(suggestion =>{
+    Object.entries(req.body).map((suggestion) => {
       [key, value] = suggestion;
-       updateSuggestions(key,value);
-    })
-    console.log({response})
+      updateSuggestions(key, value);
+    });
+    console.log({ response });
     res.status(200).send(response.data);
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
-
 });
 
+server.post("/suggestions", async (req, res) => {
+  console.log("ADD MANUALLY")
+  try {
+    const response = await addSuggestionMenually(req.body);
+    return res.status(200).send(response.data);
+    console.log({ response });
+  } catch (err) {}
+});
 
 // Schedule the job to run at specific hours
-schedule.scheduleJob('0 8,12,14,18,20 * * *', addSuggestionsToDatabase);
-schedule.scheduleJob('30 8 * * *', addSuggestionsToDatabase);
-
+schedule.scheduleJob("0 8,12,14,18,20 * * *", addSuggestionsToDatabase);
+schedule.scheduleJob("30 8 * * *", addSuggestionsToDatabase);
 
 // --------------------------------- Running the ML script ---------------------------------
 
 // const BAYESIAN_SCRIPT_INTERVAL = 600000; // 10 minutes in milliseconds
 // setInterval(runBayesianScript, BAYESIAN_SCRIPT_INTERVAL);
 
-
-
 // getHeaterState();
-
 
 // setInterval(async() => {
 //     // removeAllSensorValues();
@@ -320,7 +349,5 @@ schedule.scheduleJob('30 8 * * *', addSuggestionsToDatabase);
 //     await getFunctionsFromDB();
 
 // }, 20000);
-
-
 
 server.listen(port, () => console.log(`listening on port ${port}`));
--- suggestions.service.js
+++ suggestions.service.js
@@ -1,7 +1,7 @@
-const Suggestion = require('./Suggestion');
-const axios = require('axios');
-const { getLatestSensorValues } = require('./sensorValues.service');
-const { getCurrentSeasonAndHour } = require('./time.service');
+const Suggestion = require("./Suggestion");
+const axios = require("axios");
+const { getLatestSensorValues } = require("./sensorValues.service");
+const { getCurrentSeasonAndHour } = require("./time.service");
 
 const getSuggestions = async () => {
   try {
@@ -20,19 +20,25 @@ async function addSuggestionsToDatabase() {
   const currentHumidity = latestSensorValues.humidity;
   const currentDistance = latestSensorValues.distance;
 
-  const devices = ['lights', 'fan', 'ac_status', 'heater_switch', 'laundry_machine'];
+  const devices = [
+    "lights",
+    "fan",
+    "ac_status",
+    "heater_switch",
+    "laundry_machine",
+  ];
   const evidence = {
-    'temperature': currentTemperature,
-    'humidity': currentHumidity,
-    'distance_from_house': currentDistance,
-    'season': season,
-    'hour':hour
+    temperature: currentTemperature,
+    humidity: currentHumidity,
+    distance_from_house: currentDistance,
+    season: season,
+    hour: hour,
   };
 
   // Call the recommend_device function with the evidence
-  const response = await axios.post('http://localhost:5000/recommend_device', {
+  const response = await axios.post("http://localhost:5000/recommend_device", {
     devices: devices,
-    evidence: evidence
+    evidence: evidence,
   });
 
   const recommendedDevices = response.data;
@@ -40,18 +46,31 @@ async function addSuggestionsToDatabase() {
   // Add the suggestions to the MongoDB database
   for (const recommendedDevice of recommendedDevices) {
     const suggestion = new Suggestion({
+      id: Math.floor(10000000 + Math.random() * 90000000),
       device: recommendedDevice.device,
       evidence: {
         Temperature: evidence.temperature,
         distance: evidence.distance_from_house,
-        humidity: evidence.humidity
+        humidity: evidence.humidity,
       },
-      state: "on"
+      state: "on",
     });
     await suggestion.save();
   }
 }
 
+const addSuggestionMenually = async (suggestion) => {
+  try {
+    console.log({ suggestion });
+    const newSuggestion = new Suggestion(suggestion);
+    newSuggestion.id = Math.floor(10000000 + Math.random() * 90000000);
+    console.log({ newSuggestion });
+    const response = await newSuggestion.save();
+    return { statusCode: 200, data: response.data };
+  } catch (err) {
+    return { statusCode: 404, data: "Cant Add Suggestion - " + err.message };
+  }
+};
 
 const updateSuggestions = async (key, value) => {
   try {
@@ -69,8 +88,9 @@ const updateSuggestions = async (key, value) => {
   }
 };
 
-
 module.exports = {
   getSuggestions,
-  addSuggestionsToDatabase
+  addSuggestionsToDatabase,
+  addSuggestionMenually,
+  updateSuggestions
 };
