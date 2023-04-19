30293bda97726c3eb640032d27dde2428ba248ca
harelfogel <fogell06@gmail.com>
2023-04-19 16:54:33 +0300

    updating temprature and modes in db

--- devices.service.js
+++ devices.service.js
@@ -12,8 +12,29 @@ const getDevices = async () => {
     }
 }
 
+const updateDeviceModeInDatabase = async (deviceId, mode) => {
+    try {
+        console.log(deviceId);
+        console.log(mode)
+      const result = await Device.updateOne({ device_id: deviceId }, { $set: { mode } });
+      console.log(result)
+  
+      if (result.modifiedCount === 1) {
+        console.log('Device mode updated successfully in the database');
+        return true;
+      } else {
+        console.log('No device was found with the provided device_id');
+        return false;
+      }
+    } catch (error) {
+      console.error('Error updating device mode in the database:', error);
+      return false;
+    }
+  };
+
 
 
 module.exports = {
-    getDevices
+    getDevices,
+    updateDeviceModeInDatabase
 }
