849076769645082a922a3971f47fb1193fe7d4e0
Yovel Elad <yuvel.elad@gmail.com>
2023-04-23 14:10:48 +0300

    add-duration-support-to-sensibo-from-rule

--- sensibo.js
+++ sensibo.js
@@ -1,94 +1,126 @@
 const { default: axios } = require("axios");
 const Device = require("./Device");
 const { updateDeviceModeInDatabase } = require("./devices.service");
-const SensorValue = require('./SensorValue');
+const SensorValue = require("./SensorValue");
 
 const test = 0;
 const analyzeFunc = async (func) => {
   try {
-    let response;
-    const state = func.split(' ')[1];
-
-    if (state !== 'on' && state !== 'off') {
-      throw new Error('Invalid state');
+    const forPattern = /\b(for)\b/;
+    const isWithDuration = forPattern.test(func);
+    let durationString;
+    let time, units, durationInMiliSeconds;
+    if (isWithDuration) {
+      durationString = func.split(" for ")[1];
+      time = parseInt(durationString.substring(0, durationString.indexOf(" ")));
+      units = durationString.split(" ")[1];
+      if(units !== "minutes" && units !== "hours"){
+        throw new Error("Units has to be minutes or hours");
+      }
+      durationInMiliSeconds =
+        units === "minutes" ? time * 60 * 1000 : time * 60 * 60 * 1000;
     }
-    const boolState = state === 'on';
-    const numberPattern = /\d+/;
+
+    let response;
+    const state = func.split(" ")[1];
+
+    if (state !== "on" && state !== "off") {
+      throw new Error("Invalid state");
+    }
+    const boolState = state === "on";
+    const numberPattern = /\d+/g;
     const matches = func.match(numberPattern);
-    if (matches && matches.length > 0) {
-      const degrees = matches[0];
+    console.log({ matches });
+    let degrees = null;
+    if (
+      matches &&
+      (matches.length > 1 || (matches.length > 0 && !isWithDuration))
+    ) {
+      degrees = matches[0];
+    }
+    console.log({ degrees });
+    if (degrees) {
+      // const degrees = matches[0];
       if (degrees > 30 || degrees < 16) {
-        throw new Error('Degrees has to be between 16 and 30');
+        throw new Error("Degrees has to be between 16 and 30");
       }
       response = await switchAcState(boolState, parseInt(degrees));
     } else {
       response = await switchAcState(boolState);
     }
+    if(isWithDuration){
+      //turn ac off after a duration
+      setTimeout( async () => {
+        console.log('Turning off');
+        response = await switchAcState(false);
+        console.log({response})
+      }, durationInMiliSeconds)
+    }
     return response;
-
   } catch (err) {
-    return { statusCode: 403, data: err.message }
+    console.log(err);
+    return { statusCode: 403, data: err.message };
   }
-}
-
+};
 
 const validateDegree = (degree) => {
   if (degree > 30 || degree < 16) {
     return false;
   }
   return true;
-}
+};
 
 const switchAcState = async (state, temperature = null) => {
   // console.log("switchAcState")
   // console.log({state, temperature})
-
+  console.log({ temperature });
   try {
-    if (validateDegree(temperature)) {
-      const response = await axios.post(`https://home.sensibo.com/api/v2/pods/${process.env.SENSIBO_DEVICE_ID}/acStates?apiKey=${process.env.SENSIBO_API_KEY}`, {
-        "acState": {
-          "on": state,
-          "targetTemperature": temperature
+    if (!temperature || validateDegree(temperature)) {
+      const response = await axios.post(
+        `https://home.sensibo.com/api/v2/pods/${process.env.SENSIBO_DEVICE_ID}/acStates?apiKey=${process.env.SENSIBO_API_KEY}`,
+        {
+          acState: {
+            on: state,
+            targetTemperature: temperature,
+          },
         }
-      })
-      await Device.updateOne({ device_id: '9EimtVDZ' }, { state: state ? 'on' : 'off' });
+      );
+      await Device.updateOne(
+        { device_id: "9EimtVDZ" },
+        { state: state ? "on" : "off" }
+      );
       return { statusCode: 200, data: response.data.result };
+    } else {
+      throw new Error("Temperature has to be between 16 and 30");
     }
-    else {
-      throw new Error('Temperature has to be between 16 and 30');
-    }
-
   } catch (err) {
     // console.log(err).message;
     return { statusCode: 403, data: err.message };
   }
-
-}
-
+};
 
 const getAcState = async () => {
   try {
-    const response = await axios.get(`https://home.sensibo.com/api/v2/pods/${process.env.SENSIBO_DEVICE_ID}/acStates?apiKey=${process.env.SENSIBO_API_KEY}`)
+    const response = await axios.get(
+      `https://home.sensibo.com/api/v2/pods/${process.env.SENSIBO_DEVICE_ID}/acStates?apiKey=${process.env.SENSIBO_API_KEY}`
+    );
     const state = response.data.result[0].acState;
     return state;
   } catch (err) {
     console.log(err + " Invalid read from Sensibo");
   }
-}
-
+};
 
 const getSensiboSensors = async () => {
   try {
-    const response = await axios.get(`https://home.sensibo.com/api/v2/pods/${process.env.SENSIBO_DEVICE_ID}/measurements?fields=temperature,humidity&apiKey=${process.env.SENSIBO_API_KEY}`);
+    const response = await axios.get(
+      `https://home.sensibo.com/api/v2/pods/${process.env.SENSIBO_DEVICE_ID}/measurements?fields=temperature,humidity&apiKey=${process.env.SENSIBO_API_KEY}`
+    );
     return response;
-
   } catch (err) {
     console.log(err);
   }
-}
-
-
-
+};
 
 const parseSensorAndWriteToMongo = async () => {
   try {
@@ -99,8 +131,14 @@ const parseSensorAndWriteToMongo = async () => {
     const { temperature, humidity } = data;
     const temperatureValue = `VAR temperature=${temperature.toFixed(1)}`;
     const humidityValue = `VAR humidity=${humidity.toFixed(1)}`;
-    const temperatureDocument = new SensorValue({ value: temperatureValue, sensor_type: 'temperature' });
-    const humidityDocument = new SensorValue({ value: humidityValue, sensor_type: 'humidity' });
+    const temperatureDocument = new SensorValue({
+      value: temperatureValue,
+      sensor_type: "temperature",
+    });
+    const humidityDocument = new SensorValue({
+      value: humidityValue,
+      sensor_type: "humidity",
+    });
 
     await Promise.all([temperatureDocument.save(), humidityDocument.save()]);
 
@@ -110,11 +148,9 @@ const parseSensorAndWriteToMongo = async () => {
   }
 };
 
-
-
 const removeAllSensorValues = async () => {
   try {
-    console.log(`removeAllSensorValues`)
+    console.log(`removeAllSensorValues`);
     const result = await SensorValue.deleteMany({});
     //console.log(`Removed ${result.deletedCount} documents from the sensor_values collection.`);
   } catch (error) {
@@ -139,28 +175,27 @@ const updateAcMode = async (mode) => {
   }
 };
 
-
 const updateSensiboMode = async (deviceId, mode) => {
   try {
-    const response = await axios.post(`https://home.sensibo.com/api/v2/pods/${deviceId}/acStates?apiKey=${process.env.SENSIBO_API_KEY}`, {
-      acState: {
-        on: true,
-        mode,
-      },
-    });
+    const response = await axios.post(
+      `https://home.sensibo.com/api/v2/pods/${deviceId}/acStates?apiKey=${process.env.SENSIBO_API_KEY}`,
+      {
+        acState: {
+          on: true,
+          mode,
+        },
+      }
+    );
 
     console.log(deviceId);
-    const updateDB=await updateDeviceModeInDatabase(deviceId,mode);
-    if((response.status==200) && updateDB)
-    return { success: true, data: response.data };
+    const updateDB = await updateDeviceModeInDatabase(deviceId, mode);
+    if (response.status == 200 && updateDB)
+      return { success: true, data: response.data };
   } catch (error) {
-    console.error('Error updating Sensibo mode:', error);
-    return { success: false, message: 'Error updating mode' };
+    console.error("Error updating Sensibo mode:", error);
+    return { success: false, message: "Error updating mode" };
   }
 };
-
-
-
 
 module.exports = {
   switchAcState,
@@ -170,5 +205,5 @@ module.exports = {
   removeAllSensorValues,
   analyzeFunc,
   updateAcMode,
-  updateSensiboMode
-}
+  updateSensiboMode,
+};
