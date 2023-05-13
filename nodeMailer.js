1877a4a32b49aba621974fdefc7d769c7788b77d
harelfogel <fogell06@gmail.com>
2023-05-13 16:19:40 +0300

    adding email to user resrictions

--- nodeMailer.js
+++ nodeMailer.js
@@ -2,20 +2,21 @@ const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 const sendEmail = async (subject, text) => {
+  console.log('send email is called!')
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       type: 'OAuth2',
-      user: process.env.ADMIN_MAIL,
+      user: process.env.SYSTEM_MAIL,
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // refreshToken: process.env.REFRESH_TOKEN,
-      accessToken: process.env.GOOGLE_TOKEN,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      accessToken: process.env.GOOGLE_ACCESS_TOKEN,
     },
   });
 
   const mailOptions = {
-    from: process.env.ADMIN_MAIL,
+    from: process.env.SYSTEM_MAIL,
     to: process.env.ADMIN_MAIL,
     subject,
     text,
