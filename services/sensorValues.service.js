43c6f66602bff353b73d6e74056172ced6c6ac97
Yovel Elad <yuvel.elad@gmail.com>
2023-05-29 20:29:23 +0300

    use lodash

--- services/sensorValues.service.js
+++ services/sensorValues.service.js
@@ -1,11 +1,12 @@
 const SensorValue = require('../models/SensorValue');
+const _ = require("lodash");
 
 async function getLatestSensorValues() {
     try {
         const temperature = await SensorValue.findOne({ sensor_type: 'temperature' }).sort({ timestamp: -1 }).exec();
         const humidity = await SensorValue.findOne({ sensor_type: 'humidity' }).sort({ timestamp: -1 }).exec();
         const distance = await SensorValue.findOne({ sensor_type: 'distance' }).sort({ timestamp: -1 }).exec();
-        return { temperature: temperature.value, humidity: humidity.value, distance: distance.value };
+        return { temperature: _.get(temperature,'value'), humidity: _.get(humidity,'value'), distance: _.get(distance,'value') };
     } catch (error) {
         console.error(`Error fetching latest temperature and humidity and distance: ${error}`);
         throw error;
