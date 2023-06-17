b6857269c3f80a7089bc299ae49f2c167130b6b7
aavidan <amir.avidan@beyondminds.ai>
2023-06-17 11:45:51 +0300

    Adding soil and fix suggestion

--- services/mqtt.service.js
+++ services/mqtt.service.js
@@ -1,71 +1,71 @@
-// const mqtt = require('mqtt');
-// require('dotenv').config();
+const mqtt = require('mqtt');
+require('dotenv').config();
 
-// let latestSoilMoisture = null;
+let latestSoilMoisture = null;
 
-// // Configure the connection options
-// const options = {
-//     username: process.env.MQTT_USERNAME,
-//     password: process.env.MQTT_PASSWORD,
-// };
+// Configure the connection options
+const options = {
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD,
+};
 
-// // Create a client instance
-// const client = mqtt.connect('mqtts://' + process.env.MQTT_URL, options);
+// Create a client instance
+const client = mqtt.connect('mqtts://' + process.env.MQTT_URL, options);
 
-// client.once('connect', function () {
-//     console.log('MQTT connected');
-//     client.subscribe('esp32/soilMoisture');
-// });
-
-
-// setInterval(() => {
-//     client.once('message', function (topic, message) {
-//         if (topic == 'esp32/soilMoisture') {
-//             // console.log(`Received soil moisture: ${message.toString()}`);
-//             latestSoilMoisture = message.toString();
-//         }
-//     });
-// },2000)
-
-// client.once('error', function (error) {
-//     console.error('MQTT error: ', error);
-// });
+client.once('connect', function () {
+    console.log('MQTT connected');
+    client.subscribe('esp32/soilMoisture');
+});
 
 
+setInterval(() => {
+    client.once('message', function (topic, message) {
+        if (topic == 'esp32/soilMoisture') {
+            // console.log(`Received soil moisture: ${message.toString()}`);
+            latestSoilMoisture = message.toString();
+        }
+    });
+},2000)
 
-// // A helper function for publishing LED control messages
-// function controlLED(color, state) {
-//     const topic = 'ledControl';
-//     const message = JSON.stringify({ color: color, state: state });
-
-//     client.publish(topic, message, function (err) {
-//         if (err) {
-//             console.error('Failed to send message', err);
-//         } else {
-//             console.log(`LED control message sent: ${message}`);
-//         }
-//     });
-// }
-
-// function controlPump(state) {
-//     const topic = 'esp32/pumpControl';
-//     const message = state;
-
-//     console.log(`Sending message: ${message} to topic: ${topic}`);  // Add this line
-
-//     client.publish(topic, message, function (err) {
-//         if (err) {
-//             console.error('Failed to send message', err);
-//         } else {
-//             console.log(`Pump control message sent: ${message}`);
-//         }
-//     });
-// }
+client.once('error', function (error) {
+    console.error('MQTT error: ', error);
+});
 
 
-// module.exports = {
-//     controlLED,
-//     controlPump,
-//     getLatestSoilMoisture: () => latestSoilMoisture
 
-// }
+// A helper function for publishing LED control messages
+function controlLED(color, state) {
+    const topic = 'ledControl';
+    const message = JSON.stringify({ color: color, state: state });
+
+    client.publish(topic, message, function (err) {
+        if (err) {
+            console.error('Failed to send message', err);
+        } else {
+            console.log(`LED control message sent: ${message}`);
+        }
+    });
+}
+
+function controlPump(state) {
+    const topic = 'esp32/pumpControl';
+    const message = state;
+
+    console.log(`Sending message: ${message} to topic: ${topic}`);  // Add this line
+
+    client.publish(topic, message, function (err) {
+        if (err) {
+            console.error('Failed to send message', err);
+        } else {
+            console.log(`Pump control message sent: ${message}`);
+        }
+    });
+}
+
+
+module.exports = {
+    controlLED,
+    controlPump,
+    getLatestSoilMoisture: () => latestSoilMoisture
+
+}
